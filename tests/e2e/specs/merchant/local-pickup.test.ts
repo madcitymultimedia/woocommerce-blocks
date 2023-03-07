29fcc894e256db91c443b9175757388fc780e1d8
Thomas Roberts <thomas.roberts@automattic.com>
2023-03-07 11:36:25 -0800

    Add tests to ensure core shipping settings update correctly

--- tests/e2e/specs/merchant/local-pickup.test.ts
+++ tests/e2e/specs/merchant/local-pickup.test.ts
@@ -6,11 +6,6 @@ import { findLabelWithText } from '@woocommerce/blocks-test-utils';
 import WooCommerceRestApi from '@woocommerce/woocommerce-rest-api';
 import { default as axios } from 'axios';
 
-/**
- * Internal dependencies
- */
-import { setupPageSettings } from '../../fixtures/fixture-loaders';
-
 const goToSettingsPage = async () => {
 	await visitAdminPage(
 		'admin.php',
@@ -139,6 +134,55 @@ describe( `Local Pickup Settings`, () => {
 
 	it( 'renders without crashing', async () => {
 		await expect( page ).toMatchElement( '#local-pickup-settings' );
+	} );
+
+	describe( 'Core Settings', () => {
+		afterAll( async () => {
+			await setCartCheckoutPages( {
+				cartSlug: 'cart-block',
+				checkoutSlug: 'checkout-block',
+			} );
+		} );
+		it( 'hides the correct shipping options if Checkout block is the default', async () => {
+			await visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=shipping&section=options'
+			);
+			const hideShippingLabel = await findLabelWithText(
+				'Hide shipping costs until an address is entered'
+			);
+			expect( hideShippingLabel ).toBeUndefined();
+
+			const shippingCalculatorLabel = await findLabelWithText(
+				'Enable the shipping calculator on the cart page'
+			);
+			expect( shippingCalculatorLabel ).toBeUndefined();
+		} );
+
+		it( 'does not hide the relevant setting if Cart or Checkout block is not the default', async () => {
+			await setCartCheckoutPages( {
+				cartSlug: 'cart',
+				checkoutSlug: 'checkout',
+			} );
+
+			await visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=advanced'
+			);
+			await visitAdminPage(
+				'admin.php',
+				'page=wc-settings&tab=shipping&section=options'
+			);
+			const hideShippingLabel = await page.$x(
+				'//label[contains(., "Hide shipping costs until an address is entered")]'
+			);
+			await expect( hideShippingLabel ).toHaveLength( 1 );
+
+			const shippingCalculatorLabel = await page.$x(
+				'//label[contains(., "Enable the shipping calculator on the cart page")]'
+			);
+			await expect( shippingCalculatorLabel ).toHaveLength( 1 );
+		} );
 	} );
 
 	describe( 'Global Settings', () => {
