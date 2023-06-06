962469f4b1ab732ea0300c9273794179507c0ae5
Thomas Roberts <thomas.roberts@automattic.com>
2023-06-06 17:42:59 +0100

    Add Page to EditorUtils class

--- tests/e2e-pw/utils/editor/EditorUtils.ts
+++ tests/e2e-pw/utils/editor/EditorUtils.ts
@@ -2,14 +2,17 @@
  * External dependencies
  */
 import { Editor } from '@wordpress/e2e-test-utils-playwright';
+import { Page } from '@playwright/test';
 /**
  * Internal dependencies
  */
 
 export class EditorUtils {
 	editor: Editor;
-	constructor( editor: Editor ) {
+	page: Page;
+	constructor( editor: Editor, page: Page ) {
 		this.editor = editor;
+		this.page = page;
 	}
 
 	async getBlockByName( name: string ) {
